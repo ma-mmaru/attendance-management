ebc6777300ebc9631430d6f2de825891667a7736
kana risaki <kana@rinanoMacBook-Air.local>
2026-06-20 22:17:28 +0900

    ルーティングの設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\AdminAttendanceController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,4 +22,9 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/attendance/clock-out', [AttendanceController::class, 'clockOut'])->name('attendance.clock-out');
     Route::post('/attendance/rest-in', [AttendanceController::class, 'restIn'])->name('attendance.rest-in');
     Route::post('/attendance/rest-out', [AttendanceController::class, 'restOut'])->name('attendance.rest-out');
+    Route::get('/attendance/list', [AttendanceController::class, 'list'])->name('attendance.list');
+});
+
+Route::middleware(['auth'])->prefix('admin')->group(function(){
+    Route::get('/attendance/list', [AdminAttendanceController::class, 'list'])->name('admin.attendance.list');
 });
